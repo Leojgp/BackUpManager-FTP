552ba42292c96b4ee7cc8cd63f06086c8431eb7d
Leojgp <leonardoljgp@gmail.com>
2025-03-04 21:05:11 +0100

    New functions added (delete, save) and saving the files that are being created in another file.

--- src/main/java/com/leojgp/backupmanager/FTPConnection.java
+++ src/main/java/com/leojgp/backupmanager/FTPConnection.java
@@ -29,12 +29,14 @@ public class FTPConnection {
             throw new IOException("Error al conectar con el servidor FTP. Credenciales incorrectas.");
         }
         clienteFTP.setFileType(FTP.BINARY_FILE_TYPE);
+        System.out.println("Conexión establecida con " + SERVIDOR);
     }
 
     public void desconectar() throws IOException {
         if (clienteFTP.isConnected()) {
             clienteFTP.logout();
             clienteFTP.disconnect();
+            System.out.println("Desconectado de " + SERVIDOR);
         }
     }
 
@@ -43,11 +45,39 @@ public class FTPConnection {
         try (InputStream is = new FileInputStream(ficheroLocal)) {
             boolean enviado = clienteFTP.storeFile(ficheroLocal.getName(), is);
             if (enviado) {
-                System.out.println("He subido el archivo " + ficheroLocal.getName() + " al servidor");
+                System.out.println("Subida exitosa: " + ficheroLocal.getName());
             } else {
-                System.err.println("No pude subir el archivo " + ficheroLocal.getName());
+                System.err.println("Fallo al subir: " + ficheroLocal.getName() + " - " + clienteFTP.getReplyString());
             }
             return enviado;
+        }
+    }
+
+    public boolean eliminarFichero(String fileName) throws IOException {
+        boolean eliminado = clienteFTP.deleteFile(fileName);
+        if (eliminado) {
+            System.out.println("Eliminado del servidor: " + fileName);
+        } else {
+            System.err.println("Fallo al eliminar: " + fileName + " - " + clienteFTP.getReplyString());
+        }
+        return eliminado;
+    }
+
+    public boolean descargarFichero(String ficheroRemoto, String pathLocal) throws IOException {
+        File localFile = new File(pathLocal);
+        File parentDir = localFile.getParentFile();
+        if (parentDir != null && !parentDir.exists()) {
+            parentDir.mkdirs();
+            System.out.println("Carpeta de descarga creada: " + parentDir.getPath());
+        }
+        try (OutputStream os = new BufferedOutputStream(new FileOutputStream(localFile))) {
+            boolean recibido = clienteFTP.retrieveFile(ficheroRemoto, os);
+            if (recibido) {
+                System.out.println("Descargado exitosamente: " + ficheroRemoto + " a " + pathLocal);
+            } else {
+                System.err.println("Fallo al descargar: " + ficheroRemoto + " - " + clienteFTP.getReplyString());
+            }
+            return recibido;
         }
     }
 
@@ -55,16 +85,33 @@ public class FTPConnection {
         File dir = new File(LOCAL_DIR);
         if (!dir.exists()) {
             dir.mkdirs();
-            System.out.println("He creado la carpeta " + LOCAL_DIR + " porque no existía");
+            System.out.println("Carpeta creada: " + LOCAL_DIR);
         }
         File fichero = new File(LOCAL_DIR + "\\" + fileName);
-        if (!fichero.exists()) {
-            try (FileWriter writer = new FileWriter(fichero)) {
-                writer.write("Este es un archivo de prueba para el FTP");
-                System.out.println("He creado el archivo " + fileName + " en " + LOCAL_DIR);
+        try (FileWriter writer = new FileWriter(fichero)) {
+            writer.write("Este es un archivo de prueba para el FTP");
+            System.out.println("Archivo creado o modificado: " + fileName);
+        }
+    }
+
+    public void modificarFichero(String fileName) throws IOException {
+        File fichero = new File(LOCAL_DIR + "\\" + fileName);
+        try (FileWriter writer = new FileWriter(fichero)) {
+            writer.write("Contenido modificado del archivo");
+            System.out.println("Archivo modificado localmente: " + fileName);
+        }
+    }
+
+    public void borrarFicheroLocal(String fileName) {
+        File fichero = new File(LOCAL_DIR + "\\" + fileName);
+        if (fichero.exists()) {
+            if (fichero.delete()) {
+                System.out.println("Archivo borrado localmente: " + fileName);
+            } else {
+                System.err.println("No pude borrar localmente: " + fileName);
             }
         } else {
-            System.out.println("El archivo " + fileName + " ya existe, no lo vuelvo a crear");
+            System.out.println("El archivo " + fileName + " no existe localmente");
         }
     }
 
@@ -72,16 +119,31 @@ public class FTPConnection {
         FTPConnection gestorFTP = new FTPConnection();
         try {
             gestorFTP.conectar();
-            System.out.println("Me he conectado al servidor FTP");
 
             String fileName = "test.txt";
+            String localPath = LOCAL_DIR + "\\" + fileName;
+
+            // Creo y subo
             gestorFTP.crearFichero(fileName);
-            gestorFTP.subirFichero(LOCAL_DIR + "\\" + fileName);
+            gestorFTP.subirFichero(localPath);
+
+            // Descargo para ver el contenido
+            gestorFTP.descargarFichero(fileName,"C:\\Users\\leona\\Documents\\2DAM\\ProyectoProcesos\\downloadsFromServer\\test_downloaded.txt");
+
+            // Modifico y subo
+            gestorFTP.modificarFichero(fileName);
+            gestorFTP.subirFichero(localPath);
+
+            // Descargo de nuevo para ver el cambio
+            gestorFTP.descargarFichero(fileName,"C:\\Users\\leona\\Documents\\2DAM\\ProyectoProcesos\\downloadsFromServer\\test_downloaded_modified.txt");
+
+            // Borro localmente y del servidor
+            gestorFTP.borrarFicheroLocal(fileName);
+            gestorFTP.eliminarFichero(fileName);
 
             gestorFTP.desconectar();
-            System.out.println("Me he desconectado del servidor FTP");
         } catch (IOException e) {
-            System.err.println("Ha ocurrido un error: " + e.getMessage());
+            System.err.println("Error: " + e.getMessage());
         }
     }
 }
