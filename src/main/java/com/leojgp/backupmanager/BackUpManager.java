0ff218ca7985c11a04827ec8c8954f645eb06824
Leojgp <leonardoljgp@gmail.com>
2025-03-04 18:44:03 +0100

    Initializing project

---
+++ src/main/java/com/leojgp/backupmanager/BackUpManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.leojgp.backupmanager;
+
+/**
+ *
+ * @author leona
+ */
+public class BackUpManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
